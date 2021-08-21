1c9f39e232c551ccdd904792b5e492201c17cceb
victorugons <victorugons@gmail.com>
2021-08-21 18:06:38 -0300

    chore: added examples for schemas' properties to the OpenAPI UI

--- src/main/java/dev/victorugons/meetingroom/exception/ErrorDetails.java
+++ src/main/java/dev/victorugons/meetingroom/exception/ErrorDetails.java
@@ -1,5 +1,6 @@
 package dev.victorugons.meetingroom.exception;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,8 +10,11 @@ import java.util.Date;
 @Getter
 public class ErrorDetails {
 
+    @Schema(example = "Resource id 1 not found.")
     private String message;
+    @Schema(example = "2001-01-01T00:00:00.000+00:00")
     private Date timeStamp;
+    @Schema(example = "uri=/api/v1/rooms/1")
     private String details;
 
     public ErrorDetails (String message, Date timeStamp, String details) {
--- src/main/java/dev/victorugons/meetingroom/model/dto/RoomDTO.java
+++ src/main/java/dev/victorugons/meetingroom/model/dto/RoomDTO.java
@@ -1,6 +1,7 @@
 package dev.victorugons.meetingroom.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,13 +11,18 @@ import javax.validation.constraints.NotNull;
 @Setter
 public class RoomDTO {
 
+    @Schema(example = "1")
     private long id;
+    @Schema(example = "RoomName")
     @NotNull
     private String name;
+    @Schema(example = "01/01/2000")
     @NotNull
     private String date;
+    @Schema(example = "00:00")
     @NotNull
     private String startHour;
+    @Schema(example = "00:00")
     @NotNull
     private String endHour;
 }

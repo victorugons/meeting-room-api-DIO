9af1de8a70b4703936c9bf2aec31edabc954f407
victorugons <victorugons@gmail.com>
2021-08-21 18:02:01 -0300

    feat: create the config file for the OpenAPI specification

---
+++ src/main/java/dev/victorugons/meetingroom/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package dev.victorugons.meetingroom;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("Meeting Rooms API")
+                        .description("REST API for meeting rooms management")
+                        .version("1.0.0")
+                        .termsOfService("http://swagger.io/terms")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")));
+    }
+}
